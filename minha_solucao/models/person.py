0b25d8dea7de40e6c681aca25a338554b798060d
dmeneguin <meneguin.daniel@gmail.com>
2023-10-25 01:46:47 -0300

    added length validations on strings of payload

--- minha_solucao/models/person.py
+++ minha_solucao/models/person.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel,field_validator
+from pydantic import BaseModel,field_validator, StringConstraints
+from typing_extensions import Annotated
 from datetime import date,datetime
 from typing import Union
 
 class Person(BaseModel):
-    apelido: str
-    nome: str
+    apelido: Annotated[str,StringConstraints(max_length=32)]
+    nome: Annotated[str, StringConstraints(max_length=100)]
     nascimento: date
-    stack: Union[list[str], None]
+    stack: Union[list[Annotated[str, StringConstraints(max_length=32)]], None]
 
     @field_validator('nascimento', mode='before')
     @classmethod
